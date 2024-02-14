d44f512778227807a2058bf22c5289c5f9345f79
Eric Yap <ericy2296@gmail.com>
2024-02-14 15:45:09 -0800

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -4,12 +4,27 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  messageID int PRIMARY KEY NOT NULL,
+  message text NOT NULL,
+  userID int NOT NULL,
+  roomID int NOT NULL
+
 );
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE users (
+  /* Describe your table here.*/
+  id int PRIMARY KEY NOT NULL,
+  username varchar(15) NOT NULL
+);
 
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  id int PRIMARY KEY NOT NULL,
+  roomName varchar(15) NOT NULL
 
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
